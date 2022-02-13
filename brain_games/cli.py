7d9b8184dd562ab07f6548ed87c1ff65005df10f
Алексей Фоминых <foma1994@yandex.ru>
2022-02-13 23:18:30 +0300

    Added flake8 (Wemake Python Styleguide) + fixed code.

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,6 +1,9 @@
+"""cli module."""
+
 import prompt
 
 
 def welcome_user():
+    """Greeting function."""
     name = prompt.string('May I have your name? ')
     print('Hello, {0}!'.format(name))
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-
+"""brain_games main module."""
 
 from ..cli import welcome_user
 
 
 def main():
+    """brain_games entry point."""
     print('Welcome to the Brain Games!')
     welcome_user()
 
